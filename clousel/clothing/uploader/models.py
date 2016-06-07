86dc19de20e8ae58d5ba01044aec91ea771eb89d
Yuka Matsuo <matsuo.yk@gmail.com>
2016-06-07 23:42:27 +0900

    アプリケーション細分化

---
+++ clousel/clothing/uploader/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from accounts.models import Customer
+from clothing.models import Category
+
+class Image(models.Model):
+	own = models.ForeignKey(Customer, on_delete=models.CASCADE)
+	image = models.ImageField()
+	category = models.ManyToManyField(Category)
+	has_bought = models.BooleanField()
+
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
