19f5c28e1ec77e37d42945fabeb441551a41e304
Yuka Matsuo <matsuo.yk@gmail.com>
2016-06-08 18:57:06 +0900

    既存のカテゴリを探すときに，親もチェックするように変更

--- clousel/category_import.py
+++ clousel/category_import.py
@@ -1,13 +1,13 @@
 import json
 from shop.models import Category
 
-d = json.load(open("/srv/app/data/amebafurugiya.json"))
+d = json.load(open("/srv/app/media/amebafurugiya.json"))
 
 for row in d:
 	parent = None
 	for category in row['categories']:
 		try:
-			parent = Category.objects.get(title=category)
+			parent = Category.objects.get(title=category,parent=parent)
 		except Category.DoesNotExist:
 			c = Category(title=category, parent=parent)
 			c.save()
