343b49983880bb2c8d99315b69780c0e7cb7e36c
松尾 <yuka@Yukas-MacBook-Pro.local>
2016-06-10 15:04:54 +0900

    インポート

--- clousel/uploader/models.py
+++ clousel/uploader/models.py
@@ -1,13 +1,19 @@
 from django.db import models
-from accounts.models import Customer
+from accounts.models import User
 from shop.models import Category
 
+def customer_directory_path(instance, filename):
+	return 'customer_{0}/{1}'.format(instance.own.id, filename)
+
 class UserImage(models.Model):
-	own = models.ForeignKey(Customer, on_delete=models.CASCADE)
+	own = models.ForeignKey(User, on_delete=models.CASCADE)
 	image = models.ImageField()
 	category = models.ManyToManyField(Category)
 	has_bought = models.BooleanField()
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		verbose_name_plural = 'user images'
 
 
