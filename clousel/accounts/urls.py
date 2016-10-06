046f9dd0b246120592aaaf85472ed1a6a3ce4560
Yuka Matsuo <matsuo.yk@gmail.com>
2016-10-06 21:05:31 +0900

    registration.backends.default.urls をインポートするのを clousel/urls.py から accounts/urls.py へ

--- clousel/accounts/urls.py
+++ clousel/accounts/urls.py
@@ -1,9 +1,14 @@
+import registration.backends.default.urls
 from django.conf.urls import include, url
+from django.contrib.auth.views import logout
 
 from .views import dashboard_view
-from django.contrib.auth.views import logout
 
 urlpatterns = [
     url(r'^dashboard/$', dashboard_view, name='dashboard'),
-    url(r'^logout/$', logout, {'next_page': '/'})
+    url(r'^logout/$',
+        logout,
+        {'template_name': 'registration/logout.html', 'next_page': '/'},
+        name='auth_logout'),
+    url(r'^', include('registration.backends.default.urls')),
 ]
