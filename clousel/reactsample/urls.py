04b99b97fa32a4cb2458297aafaaff153b9b939b
Yuka Matsuo <matsuo.yk@gmail.com>
2016-10-06 21:06:12 +0900

    PEP8でフォーマット

--- clousel/reactsample/urls.py
+++ clousel/reactsample/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import include, url
 
 from .views import index_view, list_view, shop_item_list_view, staticlist_view
 
-
 urlpatterns = [
     url(r'^index/$', index_view),
     url(r'^list/$', list_view),
--- clousel/uploader/urls.py
+++ clousel/uploader/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import include, url
 
 from .views import image_view
 
-
 urlpatterns = [
     url(r'^images/^(?P<owner>[0-9]+)/((?P<filename>[0-9a-z.]+))$',
         image_view, name="userimages"),
