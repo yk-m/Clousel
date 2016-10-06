5fe43554c832cd5686a66190c527005caff24087
松尾 <yuka@macbookpro.solar>
2016-10-06 21:05:31 +0900

    registration.backends.default.urls をインポートするのを clousel/urls.py から accounts/urls.py へ

--- clousel/clousel/urls.py
+++ clousel/clousel/urls.py
@@ -13,19 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-import registration
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
-from django.contrib import admin, auth
-from django.contrib.auth.views import login, logout
+from django.contrib import admin
 
 import accounts.urls
 import api.urls
 import pages.urls
 import reactsample.urls
 import uploader.urls
-
 
 urlpatterns = [
     url(r'^', include(pages.urls, namespace="pages")),
@@ -34,7 +31,6 @@ urlpatterns = [
     url(r'^media/user/', include(uploader.urls, namespace="uploads")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(api.urls)),
-    url(r'^accounts/', include('registration.backends.default.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL + 'shop_item/',
