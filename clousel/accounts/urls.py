13d3c4b519464790d7a924785970f5b15a8889cf
IshitaTakeshi <ishitah.takeshi@gmail.com>
2016-10-06 18:37:50 +0900

    urlにcallableなオブジェクトを指定

--- clousel/accounts/urls.py
+++ clousel/accounts/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import include, url
 
 from .views import dashboard_view
-
+from django.contrib.auth.views import logout
 
 urlpatterns = [
     url(r'^dashboard/$', dashboard_view, name='dashboard'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout',
-        {'next_page': '/'})
+    url(r'^logout/$', logout, {'next_page': '/'})
 ]
